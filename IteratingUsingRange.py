1ed5b3645893fd176c9d103b22bb1aa2ee3fcf29
Farwa Hussain <97787246+farwahussain@users.noreply.github.com>
2024-09-28 17:00:50 +0500

    Add files via upload

---
+++ IteratingUsingRange.py
@@ -0,0 +1,16 @@
+small_value = iter(range(3))
+
+print(next(small_value))
+print(next(small_value))
+print(next(small_value))
+
+for num in range(3):
+    print(num)
+
+googol = iter(range(10 ** 100))
+
+print(next(googol))
+print(next(googol))
+print(next(googol))
+print(next(googol))
+print(next(googol))
