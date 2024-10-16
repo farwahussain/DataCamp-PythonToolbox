def46a577145c2141a055598ed91c2f59c454469
Farwa Hussain <97787246+farwahussain@users.noreply.github.com>
2024-10-16 22:57:27 +0500

    Delete dictComp.py

--- dictComp.py
+++
@@ -1,7 +0,0 @@
-#create a dictionary with the members of the list as the keys and the length of each string as the corresponding values.
-
-fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
-
-new_fellowship = { member: len(member) for member in fellowship}
-
-print(new_fellowship)
